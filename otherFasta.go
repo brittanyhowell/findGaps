7662f8934b5e55d1e5c5f999b7c1c25828d42830
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-04 18:34:36 +0930

    counts dashes I think, I think it counts instances of '-' in every sequence. I have initiated a series of variables which actually look for a gap, so I think it might be correct. I should check how many there are.

--- otherFasta.go
+++ otherFasta.go
@@ -24,6 +24,33 @@ func main() {
 
 	sc := seqio.NewScanner(in)
 
+	// //New byte map method
+	// seqMap := make(map[string][]byte)
+	// var seqName string
+	// for _, line := range bytes.Split(fileBytes, []byte{'\n'}) {
+
+	// 	line = bytes.TrimSpace(line)
+	// 	if len(line) == 0 {
+	// 		continue
+	// 	}
+
+	// 	// Hit an identifier line
+	// 	if line[0] == '>' {
+	// 		seqName = string(line[1:])
+
+	// 	} else {
+	// 		// Append here since multi-line DNA strings are possible
+	// 		if seqName == "" {
+	// 			return fmt.Errorf("bioutils.ReadFASTAFile(): missing sequence name in FASTA file"), nil
+	// 		}
+	// 		for _, byt := range line {
+	// 			seqMap[seqName] = append(seqMap[seqName], byt)
+	// 		}
+	// 	}
+	// }
+
+	// map seq method
+
 	AllSeqs := map[string]*linear.Seq{}
 
 	for sc.Next() {
@@ -31,14 +58,43 @@ func main() {
 		AllSeqs[s.Name()] = s
 
 	}
+	// making a dash letter to compare
+	// var hyphen []byte
+	// hyphen = make([]byte, 1)
+	// hyphen[0] = '-'
+	// dash := alphabet.BytesToLetters(hyphen)
+	// fmt.Printf("dash: %v \t \n", dash)
 
-	fmt.Printf("Candidate sequence: \n %v \n", AllSeqs["CM001014.2_44580072-44585076_+"])
+	//fmt.Printf("Candidate sequence: \n %v \n", AllSeqs["CM001014.2_44580072-44585076_+"])
+	var gaps int
+	for h := range AllSeqs { // h = header
+		gaps = 0
+		//fmt.Printf("Header: %v \n", h)
 
-	for h := range AllSeqs { //___________________ h ends up being the header
-		fmt.Printf("Header: %v \n", h)
-		sequence := AllSeqs[h]
+		seq := AllSeqs[h]
+		count := seq.Len()
 
-		fmt.Printf("sequence: %v \n", sequence)
+		fmt.Printf("seq: %v \t count: %v \n", h, count)
 
+		alpha := seq.Alphabet()
+		//indexOf := alpha.LetterIndex()
+		gap := alpha.Gap()
+
+		//fmt.Printf("gap %v %v \n", gap, indexOf)
+
+		for c := 0; c < count; c++ {
+
+			l := seq.At(c).L
+			if l == gap {
+				gaps++
+			}
+
+		}
+		fmt.Printf("gaptotal: %v \n", gaps)
 	}
+
+	//sequence := AllSeqs[h]
+
+	//fmt.Printf("sequence: %v \n", sequence)
+
 }
