f303136855a4800bccf87267bc098ea3ad16f4d6
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-06 10:35:20 +0930

    Tidied lines Prints all gaps, removed count variable, deleted redundant comments

--- otherFasta.go
+++ otherFasta.go
@@ -13,7 +13,8 @@ import (
 
 func main() {
 
-	f, err := os.Open("TestSequences.fasta")
+	f, err := os.Open("cluster_425.fasta")
+	//f, err := os.Open("TestSequences.fasta")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v.", err)
 		os.Exit(1)
@@ -21,107 +22,48 @@ func main() {
 	defer f.Close()
 
 	in := fasta.NewReader(f, linear.NewSeq("", nil, alphabet.DNA))
-
 	sc := seqio.NewScanner(in)
-
-	// map seq method
-
 	AllSeqs := map[string]*linear.Seq{}
 
 	for sc.Next() {
 		s := sc.Seq().(*linear.Seq)
 		AllSeqs[s.Name()] = s
-
 	}
-
-	//fmt.Printf("Candidate sequence: \n %v \n", AllSeqs["CM001014.2_44580072-44585076_+"])
-	//
 
 	for h := range AllSeqs { // h = header
 
 		fmt.Printf("Calculating for %v \n", h)
 
 		gaps := 0
-
 		seq := AllSeqs[h]
 		alpha := seq.Alphabet()
 		gap := alpha.Gap()
 
-		count := seq.Len()
+		var start, end, gapLength int
+		state := 0
 
-		var start int
-		var end int
-		var gapLength int
-
-		status := 0
-		for c := 0; c < count; c++ {
-			//fmt.Printf("loop %v \t", c)
+		for c := 0; c < seq.Len(); c++ {
 
 			l := seq.At(c).L
 
-			if l == gap && status == 0 {
-				status = 1
+			if l == gap && state == 0 {
+				state = 1
 				start = c
-
 			}
-			if l != gap && status == 1 {
-				status = 0
+			if l != gap && state == 1 {
+				state = 0
 				end = c - 1
-				gapLength = end - start
+				gapLength = (end - start) + 1
+				gaps++
 				fmt.Printf("start: %v, \t end: %v, \t length of gap: %v \n", start, end, gapLength)
 			}
-			if l == gap && c == count-1 {
+			if l == gap && c == seq.Len()-1 {
 				end = c
-				gapLength = end - start
+				gapLength = (end - start) + 1
+				gaps++
 				fmt.Printf("start: %v, \t end: %v, \t length of gap: %v \n", start, end, gapLength)
 			}
-
-			// if l == gap {
-			// 	start := c
-			// 	limit := count - c
-			// 	fmt.Printf("count: %v \t", count)
-			// 	for g := 0; g < limit; {
-			// 		pos := g + c
-			// 		//fmt.Printf("pos: %v", pos)
-
-			// 		if pos == count-1 {
-			// 			c = count
-			// 			break
-			// 		}
-
-			// 		candidate := seq.At(pos).L
-
-			// 		//	fmt.Printf("Position of gap: %v, sequence at gaps %v\n", pos, candidate)
-			// 		if candidate != gap {
-			// 			//						fmt.Printf("pos, not gap: %v \n", pos)
-			// 			end := pos
-			// 			//					fmt.Printf("End: %v \n", end)
-			// 			c = pos
-			// 			gapLength := end - start
-
-			// 			break
-
-			// 		} else if candidate == gap {
-			// 			//						fmt.Printf("pos, gap: %v \n", pos)
-			// 			g++
-
-			// 		}
-
-			// 	}
-			// 	gaps++
-			// } else if l != gap {
-			// 	c++
-
-			//fmt.Printf("Not a gap. gaps=%v \n", gaps)
-			//}
-
 		}
-
-		fmt.Printf("Sequence length: %vbp, Number of gaps: %v  \n\n", count, gaps)
+		fmt.Printf("Sequence length: %vbp, Number of gaps: %v  \n\n", seq.Len(), gaps)
 	}
-
-	//sequence := AllSeqs[h]
-
-	//fmt.Printf("sequence: %v \n", sequence)
-
 }
