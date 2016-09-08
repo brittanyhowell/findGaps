1b8220ab3bbc718bf9e8442eb54687a4797ee2b2
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-08 10:44:07 +0930

    Add title to plot Title taken from truncated filename

--- plotIntervals.sh
+++ plotIntervals.sh
@@ -4,10 +4,34 @@
 # Working directories
 WKDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter
 SCRIPTDIR=${WKDIR}/findGaps/Scripts
-SEQDIR=${WKDIR}/splitting/AlignedClusters/sequences
-gapOUTDIR=${WKDIR}/findGaps/gapOutput/
-gapOUT=${WKDIR}/findGaps/gapOutput/gaps*.fasta
-gapGraphOUT=${WKDIR}/findGaps/pdf/
+
+ SEQDIR=${WKDIR}/splitting/AlignedClusters/sequences
+#SEQDIR=${WKDIR}/clusterOutput/AlignedClusters/sequences
+
+gapOUTDIR=${WKDIR}/findGaps/gapOutput12split_reduced/
+gapOUT=${WKDIR}/findGaps/gapOutput12split_reduced/gaps*.fasta
+gapGraphOUTDIR=${WKDIR}/findGaps/pdf12split_reduced/
+
+# Check if the folder $Plots exists
+if [ -d $gapOUTDIR ]; then
+	rm -r $gapOUTDIR 
+	mkdir $gapOUTDIR
+	echo "Gap folder exists... replacing" 
+else 
+	echo "creating Plots folder" 
+	mkdir $gapOUTDIR
+fi 
+
+# Check if gapGraphOUTDIR folder $Plots exists
+if [ -d $Plots ]; then
+	rm -r $gapGraphOUTDIR 
+	mkdir $gapGraphOUTDIR
+	echo "Plots folder exists... replacing" 
+else 
+	echo "creating Plots folder" 
+	mkdir $gapGraphOUTDIR
+fi 
+
 
 cd ${SCRIPTDIR}
 cp otherFasta.go ${SEQDIR}
@@ -26,15 +50,17 @@ rm otherFasta.go
 
 for gapFile in ${gapOUT} ; do 
 
-	filename=${gapFile%.fasta}
+	 filename=${gapFile%.fasta}
+filenameTrunc=${filename##*/}
 
 	cd ${SCRIPTDIR}
-	Rscript coverageIntervals.R ${gapFile}  ${filename}.pdf
+	Rscript coverageIntervals.R ${gapFile}  ${filename}.pdf ${filenameTrunc}
 	# Args 1: input table
-	# Args 3: output file
+	# Args 2: output file
+	# Args 3: title of plot
 done
 
 
 cd ${gapOUTDIR}
 
-mv *.pdf ${gapGraphOUT}
+mv *.pdf ${gapGraphOUTDIR}
