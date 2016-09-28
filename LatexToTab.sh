08904d04502f76f31686fb73d9b4ddc8c025e909
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-09-29 09:13:18 +0930

    chaaaaaaaanges

--- LatexToTab.sh
+++ LatexToTab.sh
@@ -2,4 +2,8 @@
 # Script to take gapSummary table and convert it into tab delimited table.
 
 
-cat gapSummary.txt | sed s/\\\\//g |  awk '{print $1 "\t" $3 "\t" $5 "\t" $7 "\t" $9 "\t" $11 "\t" $13  }' > gapsTable.txt
+# Messes up because of amphisands and whatnot
+cat gapSummary.txt | sed s/\\\\//g |  awk '{print $1 "\t" $3 "\t" $5 "\t" $7 "\t" $9 "\t" $11 "\t" $13  }' > gapsLongTable.txt
+
+
+# cat gapSummary.txt | sed s/\\\\//g |  awk '{print $1 "\t" $3 "\t" $5 "\t" $7  }' > gapsTable.txt
--- plotIntervals.sh
+++ plotIntervals.sh
@@ -5,46 +5,46 @@
 WKDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter
 SCRIPTDIR=${WKDIR}/findGaps/Scripts
 
- SEQDIR=${WKDIR}/splitting/AlignedClusters/sequences
-#SEQDIR=${WKDIR}/clusterOutput/AlignedClusters/sequences
+# SEQDIR=${WKDIR}/splitting/AlignedClusters/sequences/
+SEQDIR=${WKDIR}/clusterOutput/AlignedClusters/sequences/1
 
-gapOUTDIR=${WKDIR}/findGaps/gapOutput12split_reduced/
-gapOUT=${WKDIR}/findGaps/gapOutput12split_reduced/gaps*.fasta
-gapGraphOUTDIR=${WKDIR}/findGaps/pdf12split_reduced/
+gapOUTDIR=${WKDIR}/findGaps/gapOutput12nonsplit_reduced/
+gapOUT=${WKDIR}/findGaps/gapOutput12nonsplit_reduced/gaps*.fasta
+gapGraphOUTDIR=${WKDIR}/findGaps/pdf12nonSplit_reduced/
 
-# Check if the folder $Plots exists
-if [ -d $gapOUTDIR ]; then
-	rm -r $gapOUTDIR 
-	mkdir $gapOUTDIR
-	echo "Gap folder exists... replacing" 
-else 
-	echo "creating Plots folder" 
-	mkdir $gapOUTDIR
-fi 
+# # Check if the folder $Plots exists
+# if [ -d $gapOUTDIR ]; then
+# 	rm -r $gapOUTDIR 
+# 	mkdir $gapOUTDIR
+# 	echo "Gap folder exists... replacing" 
+# else 
+# 	echo "creating Plots folder" 
+# 	mkdir $gapOUTDIR
+# fi 
 
-# Check if gapGraphOUTDIR folder $Plots exists
-if [ -d $Plots ]; then
-	rm -r $gapGraphOUTDIR 
-	mkdir $gapGraphOUTDIR
-	echo "Plots folder exists... replacing" 
-else 
-	echo "creating Plots folder" 
-	mkdir $gapGraphOUTDIR
-fi 
+# # Check if gapGraphOUTDIR folder $Plots exists
+# if [ -d $Plots ]; then
+# 	rm -r $gapGraphOUTDIR 
+# 	mkdir $gapGraphOUTDIR
+# 	echo "Plots folder exists... replacing" 
+# else 
+# 	echo "creating Plots folder" 
+# 	mkdir $gapGraphOUTDIR
+# fi 
 
 
-cd ${SCRIPTDIR}
-cp otherFasta.go ${SEQDIR}
+# cd ${SCRIPTDIR}
+# cp otherFasta.go ${SEQDIR}
 
-cd ${SEQDIR}
+# cd ${SEQDIR}
 
-for sequence in  *.fasta ; do 
+# for sequence in  *.fasta ; do 
 
- 	go run otherFasta.go -inSequence=${sequence} -outpath=$gapOUTDIR
+#  	go run otherFasta.go -inSequence=${sequence} -outpath=$gapOUTDIR
 
-done
+# done
 
-rm otherFasta.go
+# rm otherFasta.go
 
 
 
@@ -54,7 +54,7 @@ for gapFile in ${gapOUT} ; do
 filenameTrunc=${filename##*/}
 
 	cd ${SCRIPTDIR}
-	Rscript coverageIntervals.R ${gapFile}  ${filename}.pdf ${filenameTrunc}
+	Rscript coverageIntervals.R ${gapFile}  ${filename}_A.pdf ${filenameTrunc}_A
 	# Args 1: input table
 	# Args 2: output file
 	# Args 3: title of plot
